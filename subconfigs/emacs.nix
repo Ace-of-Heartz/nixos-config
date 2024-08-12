816f26e3e219b57552e56cd08cc1e052f0b0c5a2
Ferenci Ákos Sándor <synthetace@gmail.com>
2024-08-12 09:20:16 +0200

    Added emacs.nix

--- subconfigs/emacs.nix
+++ subconfigs/emacs.nix
@@ -5,34 +5,40 @@
 
   services.emacs = {
     enable = true;
-    package = pkgs.emacs;
+    package = pkgs.emacs-unstable;
   };
 
-  services.emacs.systemPackages = with pkgs;
-  [ ...
-    ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (
-      epkgs: [epkgs.vterm]
-    ))
-    ...
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+    }))
   ];
 
-  environment.systemPackages = [
-    (pkgs.emacsWithPackagesFromUsePackage {
-      package = pkgs.emacsGit;  # replace with pkgs.emacsPgtk, or another version if desired.
-      config = path/to/your/config.el;
-      # config = path/to/your/config.org; # Org-Babel configs also supported
+  # services.emacs.systemPackages = with pkgs;
+  # [ 
+  #   ((emacsPackagesFor emacsPgtkNativeComp).emacsWithPackages (
+  #     epkgs: [epkgs.vterm]
+  #   ))
+  
+  # ];
 
-      # Optionally provide extra packages not in the configuration file.
-      extraEmacsPackages = epkgs: [
-        epkgs.use-package;
-      ];
+  # environment.systemPackages = [
+  #   (pkgs.emacsWithPackagesFromUsePackage {
+  #     package = pkgs.emacsGit;  # replace with pkgs.emacsPgtk, or another version if desired.
+  #     config = path/to/your/config.el;
+  #     # config = path/to/your/config.org; # Org-Babel configs also supported
 
-      # Optionally override derivations.
-      override = epkgs: epkgs // {
-        somePackage = epkgs.melpaPackages.somePackage.overrideAttrs(old: {
-           # Apply fixes here
-        });
-      };
-    })
-  ];
+  #     # Optionally provide extra packages not in the configuration file.
+  #     extraEmacsPackages = epkgs: [
+  #       epkgs.use-package
+  #     ];
+
+  #     # Optionally override derivations.
+  #     override = epkgs: epkgs // {
+  #       somePackage = epkgs.melpaPackages.somePackage.overrideAttrs(old: {
+  #          # Apply fixes here
+  #       });
+  #     };
+  #   })
+  # ];
 }
